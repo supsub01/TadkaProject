a09d13066e415e48ec440a1111668566f65fdf6d
Supriya <supriya.subramanian04@gmail.com>
2021-01-23 13:38:35 -0600

    edits

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
+//import logo from './logo.svg';
+//import './App.css';
+import './custom.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Herro World! ^_^
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Tadka Project! 🍛</h1>
+        <h2>Supriya, here are your recipes:</h2>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <Button size="lg" variant="dark">WHAT SHOULD I EAT FOR DINNER?</Button>
+
       </header>
     </div>
   );
