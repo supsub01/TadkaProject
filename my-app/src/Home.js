40f630ee0646f13b612f3b1bef1482078b768232
Supriya <supriya.subramanian04@gmail.com>
2021-01-27 18:11:40 -0600

    Form elements added; Needs formatting, styling and functionality

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -2,17 +2,17 @@ import App from './App';
 import Button from 'react-bootstrap/Button';
 import newRecipe from './newRecipe';
 import { BrowserRouter, Link } from 'react-router-dom';
-import Custom from './custom.scss';
+import './custom.scss';
 
 function Home() {
   return (
     <div className="App">
-        <header className="App-header">
+        <header>
         <h1>Welcome to Tadka Project! 🍛</h1>
         <h2>Supriya, here are your recipes:</h2>
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
         
-        <Link to={"newRecipe"} size="lg"  variant="outline-secondary">+ ADD RECIPE</Link>
+        <Button href={"newRecipe"} size="lg"  variant="outline-dark"> + ADD RECIPE</Button>
         <Button class="btn btn-primary btn-lg btn-block" variant="dark">WHAT SHOULD I EAT FOR DINNER?</Button>
 
         </header>
--- my-app/src/newRecipe.js
+++ my-app/src/newRecipe.js
@@ -1,20 +1,103 @@
 import App from './App';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 import React, { useState, useEffect } from 'react';
 import './custom.scss';
 
 function newRecipe() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header >
         <h1>Welcome to Tadka Project! 🍛</h1>
         <h2>Supriya, add your new recipe here:</h2>
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
         </header>
-        <body>
-        <Button href="newRecipe.js" size="lg"  variant="outline-secondary">ADD RECIPE</Button>
+        <main>
         
-        </body>
+          <div className="Recipe">
+            <Form>
+            {/* RecipeName */}
+            <Form.Group controlId="addRecipeName">
+              <Form.Row>
+                <Col sm="4">
+                  <Form.Label>Recipe Name:</Form.Label>
+                </Col>
+                <Col sm="9">
+                  <Form.Control type="text" placeholder="Enter Recipe Name" />
+                </Col>
+              </Form.Row>
+            </Form.Group>
+
+            {/* Ingredients: variable inputs 
+            <Ingredient Name> <Amount>
+            <Add another Ingredient Button>
+            */}
+            <Form.Group controlId="addRecipeIngredients">
+              <Form.Row>
+                <Col sm="3">
+                  <Form.Label>Ingredients:</Form.Label>
+                </Col>
+                <Col sm="4">
+                  <Form.Control type="text" placeholder="Enter Ingredient Name" />
+                </Col>
+                <Col sm="3.5">
+                  <Form.Control type="text" placeholder="Enter Ingredient Amount" />
+                </Col>
+                </Form.Row>
+                <Form.Row>
+                  <Button variant="outline-dark" size= "sm" className="addButton" >
+                    + Add another Ingredient
+                  </Button>
+              </Form.Row>
+            </Form.Group>
+
+            {/* Preparation: variable inputs
+            <Numbered step>
+            <Add another step Button>
+            */}
+            <Form.Group controlId="addRecipePrep">
+              <Form.Row>
+                <Col sm="3">
+                  <Form.Label>Preparation:</Form.Label>
+                </Col>
+                <Col sm="7">
+                  <Form.Control type="text" placeholder="Step 1" />
+                </Col>
+                </Form.Row>
+
+                <Form.Row>
+                <Button variant="outline-dark" size= "sm" className="addButton">
+                  + Add another Step
+                </Button>
+              </Form.Row>
+            </Form.Group>
+
+            {/* Recipe Notes*/}
+            <Form.Group controlId="addRecipeNotes">
+              <Form.Row>
+                <Col sm="3"> 
+                  <Form.Label>Recipe Notes</Form.Label>
+                </Col>
+                <Col sm="7">
+                  <Form.Control as="textarea" rows={3} placeholder="Chef's tips..."/>`
+                </Col>
+              </Form.Row>
+            </Form.Group>
+            
+
+            {/* TODO: Recipe Tags */}
+
+            {/* Add Recipe Button */}
+            <Button variant="primary" type="submit">
+              Add My Recipe
+            </Button>
+          </Form>
+
+          </div>
+          
+        
+        </main> 
     </div>
   );
 }
